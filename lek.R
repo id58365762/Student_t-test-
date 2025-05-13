b4fadc22eaa049c426d08ac7ef66a82fdac4015f
id58365762 <hhakk01@outlook.com>
2025-05-13 20:42:30 +0300

    Add files via upload

---
+++ lek.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+
+df <- read.csv("medicines.csv")
+str(df)
+df$Group = factor(df$Group)
+
+pressures_before <- data.frame(Group = df$Group, Pressure = "before", Value = df$Pressure_before)
+pressures_after <- data.frame(Group = df$Group, Pressure = "after", Value = df$Pressure_after)
+
+pressures <- rbind(pressures_before, pressures_after)
+
+ggplot(pressures, aes(Group, Value, col=Pressure))+
+  geom_boxplot()
+
+ggplot(pressures, aes(Value, fill=Group))+
+  geom_density(alpha = 0.5)+
+  facet_wrap(Pressure ~ .)
